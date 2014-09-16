9d3cdd5337c9ff11ccfc5ab67c95b828cf43a729
ypzhang <ypzhang@stoneridgetechnology.com>
2014-09-16 12:00:44 -0400

    make program xelatex

--- book/scripts/template.py
+++ book/scripts/template.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python 
+#!/usr/bin/python
 import numpy as np 
 a = np.linspace(0.,10.,100) 
 b = np.sqrt(a) 
+c = a
 import matplotlib 
 matplotlib.use('Agg') 
 import matplotlib.pyplot as plt 
 fig = plt.figure() 
 ax = fig.add_subplot(111) 
+
+ax.set_title("");
+ax.set_xlabel('x label')
+ax.set_ylabel('y label')
 fig.add_subplot(ax) 
 ax.plot(a, b, linestyle = ':', color = 'k', linewidth = 3) 
-fig.savefig('broken.pdf') 
+ax.plot(a, c, linestyle = '-', color = 'k', linewidth = 3) 
+plt.show()
+fig.savefig('broken.eps') 
