cd6b24e3c2e078abbfbce2b1add8c4f4506c4808
ypzhang <ypzhang@stoneridgetechnology.com>
2014-12-07 12:37:14 -0500

    add matrix interface. Still trying to make it flexible to run on CPU or GPU.

---
+++ book/scripts/d2d_kernel/run_all.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# d2d chapter figures
+python cumemcpy_to_direct.py
+python cumemcpy_to_direct_offset1.py
---
+++ src/sparse/matrix.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "array.h"
+
+namespace jusha {
+
+  template <typename T>
+  class Matrix {
+  public:
+    
+  private:
+    
+  };
+
+  // utilities:
+  template <typename T>
+    void AXPY(const T alpha, const Matrix<T> &x, const T beta, cuda::MirroredArray &y)
+}
+
