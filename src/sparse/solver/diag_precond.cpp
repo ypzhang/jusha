29296071cd332d529b94bfa65c63c46328a1d0cf
ypzhang <ypzhang@stoneridgetechnology.com>
2015-01-20 14:40:19 -0500

    adding diag_precond

--- src/sparse/solver/diag_precond.cpp
+++ src/sparse/solver/diag_precond.cpp
@@ -9,6 +9,7 @@ namespace jusha {
     assert(x.size() == m_diag.size());    
     JVector<double> y(x.size());
 
+    multiply(x, m_diag, y);
     // for (int i = 0; i < x.size(); i++)
     //   y(i) = x(i) * diag(i);
 
